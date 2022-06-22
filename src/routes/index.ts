04f961739c4c80b6fd320cb952c22e9025c0456b
Julio Marodin <juliocesar_cocacola@hotmail.com>
2022-06-22 18:51:30 -0300

    feat: add cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express, { Application } from "express";
 import morgan from "morgan";
 
@@ -13,6 +14,7 @@ const routes = [showsRouter, authRouter, UserRouter, EpisodeRouter, ListRouter];
 const jsonParserMiddleware = express.json();
 
 function startRoutes(app: Application) {
+  app.use(cors());
   app.use(jsonParserMiddleware);
   app.use(morgan("tiny"));
   app.use(errorHandlerMiddleware);
